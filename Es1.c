fc5b4ca2b080a308b204d74d6b9cfa0544790633
Sebastian-duque <102580281+Sebastian-duque@users.noreply.github.com>
2022-07-14 10:27:56 +0200

    Add files via upload

---
+++ Es1.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+
+int main(){
+	/*variabili*/
+	int numero;
+	int incremento;
+	int decremento;
+	/**char somma=0;**/
+	
+	incremento= numero + 1;
+	printf("inserisci un numero\n");/*Numero da inserire da parte dell'utente*/
+	scanf("%d", numero);
+	
+/*	incremento= numero + 1;*/
+	printf("Incremento: %d\n", incremento);
+	scanf("%d", incremento);
+}
